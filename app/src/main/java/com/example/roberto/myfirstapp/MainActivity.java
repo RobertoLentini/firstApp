6f77519aeb8acad8402090ada16641ef07c67f2c
Roberto Lentini <Roberto Lentini>
2017-02-10 10:40:48 +0100

    firstApp

---
+++ app/src/main/java/com/example/roberto/myfirstapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.roberto.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Roberto on 07/02/2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
